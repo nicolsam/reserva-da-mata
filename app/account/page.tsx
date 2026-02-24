64d48e3f0e21a8868869f2b0b76b0301bde7201c
Nícolas Samuel <nicolas.lorrayne@gmail.com>
2026-02-23 22:16:44 -0300

    initial commit

---
+++ app/account/page.tsx
@@ -0,0 +1,5 @@
+export default function Account() {
+    return (
+        <h1>This is the Account Page</h1> 
+    );
+}
---
+++ app/cabins/page.tsx
@@ -0,0 +1,3 @@
+export default function Cabins() {
+    return <h1>This is the Cabins Page</h1>;
+}
