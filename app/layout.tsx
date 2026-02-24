f102a5c1cc9454808b056b4371395b096cf088c7
Nícolas Samuel <nicolas.lorrayne@gmail.com>
2026-02-23 23:00:30 -0300

    feat: added title metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,12 @@ import "@/app/_styles/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Reserva da Mata",
+  title: {
+    template: '%s | Reserva da Mata',
+    default: 'Welcome | Reserva da Mata',
+  },
+  description:
+    'Reserva da Mata is a reserve ecosystem focused on natural retreats and luxury chalets in Brazil.',
 };
 
 export default function RootLayout({
@@ -15,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className="bg-blue-800">
+      <body className="bg-blue-800 text-gray-200">
 
         <header>
           <Navigation />
