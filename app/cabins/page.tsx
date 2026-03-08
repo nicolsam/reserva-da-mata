5e3dcd0eb8b3b19f8dc2afc6e36fe674a189a2df
Nícolas Samuel <nicolas.lorrayne@gmail.com>
2026-03-08 01:31:27 -0300

    feat: show all listed cabins

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,3 +1,38 @@
-export default function Cabins() {
-    return <h1>This is the Cabins Page</h1>;
+import { getAllCabins } from "@/src/services/components/CabinService";
+
+import Image from "next/image";
+
+import {
+    Card,
+    CardContent,
+    CardHeader,
+} from '@/components/ui/card';
+
+export default async function Cabins() {
+    const cabins = await getAllCabins();
+
+    return (
+        <div>
+            <h1>Cabins list</h1>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {cabins.map(cabin => (
+                    <Card key="cabin.id">
+                        <div>
+                            <Image 
+                                src={cabin.imageUrl} 
+                                alt={cabin.name} 
+                                width={400} 
+                                height={200} 
+                                className="rounded-t-lg" 
+                                />
+                        </div>
+                        <CardHeader>{cabin.name}</CardHeader>
+                        <CardContent>{cabin.description}</CardContent>
+                    </Card>
+                ))}
+            
+            </div>
+        </div>
+    )
 }
