3a50285e0b7898085fe2381acc10ab68a9f2f6c3
Nícolas Samuel <nicolas.lorrayne@gmail.com>
2026-03-07 23:07:04 -0300

    feat: Added dynamic title using Next.js Metadata

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,3 +1,9 @@
+
+export const metadata = {
+    title: 'Account | Reserva da Mata',
+    description: 'Manage your account settings and preferences.',
+};
+
 export default function Account() {
     return (
         <h1>This is the Account Page</h1> 
